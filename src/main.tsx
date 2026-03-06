c170956c4243e7a773445b29b577f8511de23469
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-03-07 00:04:00 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import DoublyReinforcedTool from './DoublyReinforced'; // Import your new tool
-import './style.css'; //
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DoublyReinforced from './DoublyReinforced'
+import './style.css'
 
-// If you want this to be the main page when the site opens:
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <DoublyReinforcedTool />
-  </React.StrictMode>
-);
+    <DoublyReinforced />
+  </React.StrictMode>,
+)
