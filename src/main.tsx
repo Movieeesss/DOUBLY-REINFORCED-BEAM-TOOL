23b931d75bcc933647252a0fb48a9f4457887b5d
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-03-07 01:33:53 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import DoublyReinforced from './DoublyReinforced'
+import DoublyReinforcedTool from './DoublyReinforced'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <DoublyReinforced />
+    <DoublyReinforcedTool />
   </React.StrictMode>,
 )
+
+// Register Service Worker for "Add to Home Screen"
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(reg => {
+      console.log('SW Registered!', reg);
+    }).catch(err => {
+      console.log('SW Failed!', err);
+    });
+  });
+}
