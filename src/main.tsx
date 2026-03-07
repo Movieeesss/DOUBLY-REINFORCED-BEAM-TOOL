dc7accd2722a7ca9100273d060af045124dcbbf9
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-03-07 10:33:29 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import DoublyReinforcedTool from './DoublyReinforced'
+import { DoublyReinforcedTool } from './DoublyReinforced'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -9,13 +9,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   </React.StrictMode>,
 )
 
-// This registers the logic that tells Chrome to show the INSTALL button
+// Register Service Worker for PWA Installation
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(reg => {
-      console.log('App is ready for installation');
-    }).catch(err => {
-      console.log('Installation logic failed', err);
-    });
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('SW Registered!', reg))
+      .catch(err => console.log('SW Registration Failed', err));
   });
 }
